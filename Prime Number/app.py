da7b032e72e2dc550551a63c426c88a2407d1ef6
Mahdi EttehadNejad <99660553+mahdi-Eth@users.noreply.github.com>
2022-08-30 08:03:05 +0430

    Add files via upload

---
+++ Prime Number/app.py
@@ -0,0 +1,15 @@
+def primer_check(n):
+    prime_state = True
+    for i in range(2,n+1):
+        if i == 2:
+            print(2)
+            continue
+        for j in range(2,i):
+            if i % j == 0:
+                prime_state = False
+                break
+        if prime_state: print(i)
+
+n = int(input("Enter a number to see the prime numbers up to that number : "))
+primer_check(n)
+
